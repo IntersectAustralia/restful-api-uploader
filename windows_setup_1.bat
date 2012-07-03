5f040f7fcd5c3b3f543953d84f0de2ec89855877
Georgina Edwards <gc_edwards@yahoo.com.au>
2012-07-03 11:30:57 +1000

    Windows setup tweaks

--- windows_setup_1.bat
+++ windows_setup_1.bat
@@ -1,3 +1,1 @@
 rubyinstaller\rubyinstaller-1.9.3-p194.exe /verysilent /tasks="assocfiles,modpath"
-copy sample_config.yml ..\config.yml /Y
-copy windows_api_load.bat ..\windows_api_load.bat /Y
--- windows_setup_2.bat
+++ windows_setup_2.bat
@@ -1,2 +1,6 @@
+copy sample_config.yml ..\config.yml /Y
+
+echo ruby "%~dp0lib\run_batch.rb" "%~dp0..\config.yml">..\windows_api_load.bat
+
 gem install bundler --version 1.1.4
 bundle install --without=development
