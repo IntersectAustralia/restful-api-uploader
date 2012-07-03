05d0af9074995286750954ca51539ffa0255eb82
Georgina Edwards <gc_edwards@yahoo.com.au>
2012-07-03 11:13:45 +1000

    Windows setup tweaks

--- windows_setup.bat
+++
@@ -1,5 +0,0 @@
-rubyinstaller\rubyinstaller.exe /verysilent /tasks="assocfiles,modpath"
-copy sample_config.yml ..\config.yml /Y
-copy windows_api_load.bat ..\windows_api_load.bat /Y
-gem install bundler --version 1.1.4
-bundle install --without=development
---
+++ windows_setup_1.bat
@@ -0,0 +1,3 @@
+rubyinstaller\rubyinstaller-1.9.3-p194.exe /verysilent /tasks="assocfiles,modpath"
+copy sample_config.yml ..\config.yml /Y
+copy windows_api_load.bat ..\windows_api_load.bat /Y
