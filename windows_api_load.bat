d8493bc71a7421565640410f014331cf7cd5b92e
Georgina Edwards <gc_edwards@yahoo.com.au>
2012-07-03 10:00:24 +1000

    Updated sample config and example windows batch script

---
+++ windows_api_load.bat
@@ -0,0 +1,3 @@
+REM modify the paths below to match the location where you have installed the scripts and the location of your config file
+
+ruby "C:\Documents and Settings\Administrator\My Documents\dc21\IntersectAustralia-restful-api-uploader-5c265ec\lib\run_batch.rb" "C:\Documents and Settings\Administrator\My Documents\dc21\config.yml"
