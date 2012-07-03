754297b52f67d61e0d924ed4b8585e45a0ec4ef8
Georgina Edwards <gc_edwards@yahoo.com.au>
2012-07-03 13:10:51 +1000

    Windows setup tweaks

--- windows_setup_2.bat
+++ windows_setup_2.bat
@@ -1,6 +1,4 @@
 copy sample_config.yml ..\config.yml /Y
-
 echo ruby "%~dp0lib\run_batch.rb" "%~dp0..\config.yml">..\windows_api_load.bat
+gem install rest-client --version 1.6.7
 
-gem install bundler --version 1.1.4
-bundle install --without=development
