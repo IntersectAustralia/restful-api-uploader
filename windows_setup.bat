9b58b2cc670f5c92d72e9384730f2c88d4eb9a44
Georgina Edwards <gc_edwards@yahoo.com.au>
2012-07-03 10:54:51 +1000

    Setup script with ruby installer

--- windows_setup.bat
+++ windows_setup.bat
@@ -1,2 +1,5 @@
+rubyinstaller\rubyinstaller.exe /verysilent /tasks="assocfiles,modpath"
+copy sample_config.yml ..\config.yml /Y
+copy windows_api_load.bat ..\windows_api_load.bat /Y
 gem install bundler --version 1.1.4
 bundle install --without=development
