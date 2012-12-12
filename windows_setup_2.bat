ce4886d10618699c3c82317f0ddc18830108c766
Elyse <elyse@intersect.org.au>
2012-12-12 15:40:59 +1100

    --amend

--- windows_setup_2.bat
+++ windows_setup_2.bat
@@ -1,5 +1,5 @@
 copy sample_config.yml ..\config.yml /Y
-echo ruby "%~dp0lib\run_wrapper.rb" "%~dp0..\config.yml">..\windows_api_load.bat
+echo ruby "%~dp0lib\run_wrapper.rb" "%~dp0..\wrapper_config.yml" "%~dp0..\transfer_config.yml>..\windows_api_load.bat
 gem install rest-client --version 1.6.7
 pause
 
