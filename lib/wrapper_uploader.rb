79c41b1f1671816222b3b333ffbd4f557da5d008
Elyse <elyse@intersect.org.au>
2012-12-13 10:15:13 +1100

    fixing windows-specific file permissions problem and address resultion

--- lib/wrapper_uploader.rb
+++ lib/wrapper_uploader.rb
@@ -83,10 +83,12 @@ class WrapperUploader
   def get_dated_filename(src_path, rotation_date)
     unless rotation_date.nil?
       date = rotation_date.strftime("%Y%m%d")
-      existing_filename = src_path.split(File::SEPARATOR).last
+      existing_filename = src_path.tr("[\]", "/").split('/').last
       new_filename = existing_filename.split('.').first
       new_filename << "_#{date}"
-      new_filename << ".#{src_path.split('.').last}"
+      if existing_filename.includes?('.')
+        new_filename << ".#{existing_filename.split('.').last}"
+      end
     end
   end
 end
